010e51fbff5c25b2d45db2a2b24fd221faf6e06d
Nyoto <nyoto.arif@ardomusnet.com>
2018-03-21 14:21:48 +0800

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Weapon } from './weapons/weapon';
+import { WeaponDataService } from './weapons/weapon-data.service';
+import { WType } from './type/wtype';
+import { WTypeDataService } from './type/wtype-data.service';
+import { WClass } from './class/wclass';
+import { WClassDataService } from './class/wclass-data.service';
+import { GrabDataService } from './script/grab-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [WeaponDataService, WTypeDataService, WClassDataService, GrabDataService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  newWeapon: Weapon = new Weapon();
+  newType: WType = new WType();
+  newClass: WClass = new WClass();
+  selectedWeapon: Weapon;
+
+  constructor(private weaponDataService: WeaponDataService, private wTypeDataService: WTypeDataService,
+    private wClassDataService: WClassDataService, private grabDataService: GrabDataService) {
+  }
+
+  ngOnInit() {
+    this.grabDataService.fetchAndParseData()
+      .then((DataToParse) => {
+        console.log('*ba dum tss* ', DataToParse);
+      })
+      .catch(function (error) {
+        console.log('omo! ', error);
+      });
+  }
 }
